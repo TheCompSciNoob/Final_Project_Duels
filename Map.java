e9cb441a788c67a89e08b8bb9e6173afde1bea1d
Chrysopelea-Rc <kennethwang330842@gmail.com>
2017-05-18 14:14:25 -0700

    Add files via upload
    These are the files for map

---
+++ Map.java
@@ -0,0 +1,238 @@
+import java.util.ArrayList;
+public class Map
+{
+    Tile[][] map;
+    private  int mapType=(int)Math.random()*2;
+    private int rowSpawn;
+    private int colSpawn;
+    public Map()
+    {
+        map=new Tile[(int)(Math.random()*8+7)][(int)(Math.random()*5+5)];//4&6 are the min sizes of a array 
+        rowSpawn=0;
+        colSpawn=map[0].length-1;
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=0;col<map[row].length;col++)
+            {
+
+                map[row][col]=new Tile((int)(1+(Math.random()*2.)),0,0);
+
+            }
+        }
+    }
+
+    public void start()
+    {
+        int x= (map[0].length-1)*(map.length-1);
+        while (x>5)
+        {
+            genMap();
+            x/=12;
+        }
+        filBlok();
+        setEnd();
+        setKoin(map.length*map[0].length/8);
+    }
+    
+    public void genMap()
+    {
+
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=map[0].length-1;col>=map[row].length;col--)
+            {
+                int i= (int)(Math.random()*2.0);
+                if (i==3)
+                {map[row][col].setType(i);}
+
+            }
+        }
+        map[rowSpawn][colSpawn].setType(0);
+
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=map[0].length-1;col>=map[row].length-1;col--)
+            {
+                int i= (int)(Math.random()*2.0);
+                if (i==0 && nearBlank(row,col)==true)
+                {
+                    map[row][col].setType(i);
+
+                }
+
+            }
+        }
+        for (int col=map[0].length-1;col>=map[0].length-1;col--)
+        {
+            for (int row=0;row<map.length;row++)
+            {
+                int i= (int)(Math.random()*2.0);
+                if (i==0 && nearBlank(row,col)==true)
+                {
+                    map[row][col].setType(i);
+
+                }
+
+            }
+        }
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=0;col<map[row].length-1;col++)
+            {
+                int i= (int)(Math.random()*2.0);
+                if (i==0 && nearBlank(row,col)==true)
+                {
+                    map[row][col].setType(i);
+
+                }
+
+            }
+        }
+        for (int col=0;col<map[0].length;col++)
+        {
+            for (int row=0;row<map.length;row++)
+            {
+                int i= (int)(Math.random()*2.0);
+                if (i==0 && nearBlank(row,col)==true)
+                {
+                    map[row][col].setType(i);
+
+                }
+
+            }
+        }
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=0;col<map[row].length;col++)
+            {
+                int i= (int)(Math.random()*2.0);
+                if (i==0 && nearBlank(row,col)==true)
+                {
+                    map[row][col].setType(i);
+
+                }
+
+            }
+        }
+    }
+    public void setEnd()
+    {
+        int mxROW=0;
+        int mxCOL=0;
+        int mxL=0;
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=0;col<map[row].length;col++)
+            {
+                int l=Math.abs(row-rowSpawn)+Math.abs(col-colSpawn);
+                if (map[row][col].getType()==0&&l>mxL)
+                {
+                    mxL=l;
+                    mxROW=row;
+                    mxCOL=col;
+                }
+            }
+        }
+        map[mxROW][mxCOL].setType(5);
+    }
+    public void setKoin(int num)
+    {
+        while(num>0)
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=0;col<map[row].length;col++)
+            {
+                if(map[row][col].getType()=0 && (int)(Math.random()*6)=1)
+                {
+                map[row][col].setItem(1);
+                num--;
+                }
+            }
+        }
+    }
+    public boolean nearBlank(int row, int col)
+    {
+        if ( row !=0 && map[row-1][col].getType()==0 ||
+        col !=0 && map[row][col-1].getType()==0 ||
+        col != map[0].length-1 && map[row][col+1].getType()==0 ||
+        row != map.length-1 && map[row+1][col].getType()==0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void filBlok()
+    {
+        for (int row=0;row<map.length;row++)
+        {
+            for (int col=0;col<map[row].length;col++)
+            {
+
+               if ( row !=0 && col !=0 &&col != map[0].length-1 &&row!= map.length-1
+               && map[row-1][col].getType()==0 &&
+               map[row+1][col-1].getType()==0 &&
+               map[row-1][col+1].getType()==0 &&
+               map[row+1][col+1].getType()==0 &&
+               row !=0 && map[row-1][col-1].getType()==0 &&
+               col !=0 && map[row][col-1].getType()==0 &&
+               col != map[0].length-1 && map[row][col+1].getType()==0 &&
+               row != map.length-1 && map[row+1][col].getType()==0)
+               {
+                   map[row][col].setType(3);
+                }
+        
+            }
+        }
+    }
+    public void printMap()
+    {
+        for (int col=0;col<map[0].length;col++)
+        {
+            for (int row=0;row<map.length;row++)
+            {
+                System.out.print("("+map[row][col].getType()+","+map[row][col].getItem()+")");
+
+            }
+            System.out.println();
+        }
+    }           
+    public static ArrayList<GamePiece> convertToGamePiece()
+    
+    {
+        Arraylist<GamePiece> x= new Arraylist<GamePiece>();
+        for (int col=0;col<map[0].length;col++)
+        {
+            for (int row=0;row<map.length;row++)
+            {
+                if (map.getType()=1)
+                {
+                    x.add(new Wall(col*65,row*65,"Wall1"));
+                }
+                else if(map.getType()=2)
+                {
+                    x.add(new Wall(col*65,row*65,"Pit") );
+                }
+                else if(map.getType()=3)
+                {
+                    x.add(new Pit(col*65,row*65,"Wall2") );
+                }
+                else if(map.getType()=5)
+                {
+                    x.add(new Portal(col*65,row*65,"Wall2") );
+                }
+            }
+            
+        }
+        for (int col=0;col<map[0].length;col++)
+        {
+            for (int row=0;row<map.length;row++)
+            {
+                //Add tobokens
+                if (map.getItem==1)
+                {
+                    x.add(new Token(col*65,row*65,"Token"));
+                }
+            }
+        }
+    }
+}
