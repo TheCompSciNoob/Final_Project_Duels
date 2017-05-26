007b00f148722129baaa539e085ae75604ae5a28
TheCompSciNoob <kyroschow@gmail.com>
2017-05-26 12:40:22 -0700

    Add files via upload

---
+++ FrameManager.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.event.*;
+public class FrameManager extends JFrame
+{
+  //everything to keep track
+  private ArrayList<GamePiece> entities;
+  private static final int gameSpeed = 3;
+  
+  public FrameManager()
+  {
+    super();
+    entities = new ArrayList<GamePiece>();
+    makeList();
+  }
+  
+  public void displayGUI()
+  {
+    //adds the main screen and its buttons
+    add(new MainScreenManager(entities));
+    CustomButton startButton = new CustomButton(new Rectangle(50, 50, 50, 50));
+    startButton.add(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        add(new BattleManager(gameSpeed, entities));
+        revalidate();
+        startButton.setEnabled(false);
+      }
+    });
+    add(startButton);
+    //shows the frame
+    setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setTitle("GameWindow");
+    setVisible(true);
+  }
+  
+  private void makeList()
+  {
+    
+  }
+}
